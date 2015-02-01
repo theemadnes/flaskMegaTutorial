e985b78603aca60827ff5312e7de7d2a216c200e
theemadnes <alex.mattson@gmail.com>
2015-02-01 15:05:29 -0800

    done with unit testing

--- config.py
+++ config.py
@@ -7,6 +7,15 @@ db_path = os.environ['DBPATH']
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + db_user + ':' + db_password + '@' + db_path
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
+# mail server settings
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+# administrator list
+ADMINS = ['you@example.com']
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'this is my secret key'
 
